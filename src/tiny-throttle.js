1419a2ab722cd3e5527901e844956224f0d08079
Marco Bozzola <marco@basili.co>
2014-03-25 15:13:31 +0100

    **breaking**! Super refactor to make it aware of timeslot positions.

--- src/tiny-throttle.js
+++ src/tiny-throttle.js
@@ -1,31 +1,30 @@
 /**
- * Super-tiny callback wrapper + throttler.
- * Ensures that the last occurred event is always called, even if it falls between throttling threshold.
- * Optionally, it can call the function only once, at the end of a throttling timeframe.
+ * Super-tiny but powerful callback wrapper and throttler.
+ * Ensures that the last occurred event is always called, even if it falls between the throttling threshold.
+ * It can apply the original scope to the fn.
+ * It’s able to distinguish timeslot position in which the fn has been called {'head'|'body'|'tail'}
  *
  * @author: Marco Bozzola <marco@basili.co>
  *
  * @param  {Function} fn        original callback to be throttled
  * @param  {int}      threshold throttling threshold
  * @param  {scope}    scope     scope to be applied to the callback
- * @param  {bool}     tail      whether to call the callback only once, at the end of a throttling timeframe
- * @return {Function} throttled function
+ * @param  {bool}     forceTail whether to force a second fn call (with position='tail') even if the callback fired only once (in one timeslot)
+ * @return {Function} throttled function, with an added parameter to distinguish timeslot position in which the fn has been called {'head'|'body'|'tail'}
  */
-var tinyThrottle = function(fn, threshold, scope, tail) {
-  var threshold = threshold || 250;
-  var tick, deferTimer;
+var tinyThrottle = function(fn, threshold, scope, forceTail) {
+  var threshold = threshold || 250, tick = 0, deferTimer, tailed = true;
   return function() {
-    var context = scope || this;
-    var now = +new Date, args = arguments;
-    if (tail || (tick && now<tick+threshold)) {
-      clearTimeout(deferTimer);
-      deferTimer = setTimeout(function() {
-        tick = now;
-        fn.apply(context, args);
-      }, threshold);
-    } else {
-      tick = now;
-      fn.apply(context, args);
+    var context = scope || this, args = Array.prototype.slice.call(arguments), now = +new Date, truncate;
+    if (now > tick+threshold) {
+      fn.apply(context, args.concat(tailed ? 'head':'body'));
+      truncate = !forceTail && tailed;
+      tick = now; tailed = false;
     }
+    clearTimeout(deferTimer);
+    deferTimer = setTimeout(function() {
+      !truncate && fn.apply(context, args.concat('tail'));
+      tick = now; tailed = true;
+    }, threshold);
   };
 };
